6edd65be4f338bac9cf73a5c50643c4343c8e574
Jitesh <jiteshbansal41@gmail.com>
2024-04-07 18:37:25 +0530

    thirdCommit

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from flask import Flask, render_template, redirect, url_for, request, session
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
-from flask_bootstrap import Bootstrap5
-from wtforms.validators import DataRequired, URL
-from datetime import date
 import datetime
-import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+import os
+from datetime import date
+
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
-import os
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
